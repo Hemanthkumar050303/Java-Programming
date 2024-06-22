a130dd2fc31cf918a501c36942428ba389903b09
T.Hemanth Kumar <132538102+Hemanthkumar050303@users.noreply.github.com>
2024-06-22 13:35:56 +0530

    Update LogicGates.java

--- LogicGates.java
+++ LogicGates.java
@@ -2,12 +2,8 @@ public class LogicGates {
     public static void main(String[] args) {
         boolean input1 = true;
         boolean input2 = false;
-        
-        // OR gate
         boolean resultOR = input1 || input2;
         System.out.println("OR gate: " + resultOR);
-        
-        // AND gate
         boolean resultAND = input1 && input2;
         System.out.println("AND gate: " + resultAND);
     }
