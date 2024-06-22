be7512bf6350c5ced308b8caab977903e6eb6046
T.Hemanth Kumar <132538102+Hemanthkumar050303@users.noreply.github.com>
2024-06-22 13:35:25 +0530

    Update GreatestOfThree.java

--- GreatestOfThree.java
+++ GreatestOfThree.java
@@ -3,10 +3,7 @@ public class GreatestOfThree {
         int num1 = 10;
         int num2 = 30;
         int num3 = 20;
-
-        // Using nested ternary operators to find the maximum
         int greatest = (num1 > num2) ? ((num1 > num3) ? num1 : num3) : ((num2 > num3) ? num2 : num3);
-
         System.out.println("The greatest number among " + num1 + ", " + num2 + ", and " + num3 + " is: " + greatest);
     }
 }
